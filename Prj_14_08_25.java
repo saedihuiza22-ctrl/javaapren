d81a234fee5b9cb1970b0615ee32f135426742b5
saedihuiza22-ctrl <saedihuiza22@gmail.com>
2025-08-14 16:04:55 -0300

    upload do primeiro arquivo do exercicio

---
+++ Prj_14_08_25.java
@@ -0,0 +1,28 @@
+/*
+ execicio satoshi 
+ */
+package prj_14_08_25;
+import javax.swing.*;
+public class Prj_14_08_25 {
+    public static void main(String[] args) {  
+      double x; // declaração de variáveis 
+     // mensagem ao usuário e entrada de dados
+      x = Double.parseDouble(JOptionPane.showInputDialog("Digite um número real"));
+      // condicionais
+     if (x > 0)
+      {
+        System.out.println (x + " é positivo"); // saída de resultados
+      } // fim do if
+    } // fim do void main
+} // fim da classe Positivo
+
+        
+        
+        
+        
+        
+        
+        
+    
+    
+
